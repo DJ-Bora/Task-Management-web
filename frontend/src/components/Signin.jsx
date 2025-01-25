348e2d85b918156ee66340a4e1d7b048a94f4986
Dhrubajyoti Bora <92727396+DJ-Bora@users.noreply.github.com>
2025-01-25 13:41:09 +0530

    Update Signin.jsx

--- frontend/src/components/Signin.jsx
+++ frontend/src/components/Signin.jsx
@@ -28,7 +28,7 @@ const Signin = () => {
 
     try {
       const response = await axios.post(
-        `https://task-management-web-three.vercel.app/api/v1/signin`,
+        `${window.location.origin}/api/v1/signin`,
         inputs
       );
 
